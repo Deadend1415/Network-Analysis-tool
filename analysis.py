dcab06daef610858dd4f2dafb30722a3af19acaa
RoboDaniel <ahipuedaniels@gmail.com>
2025-12-20 08:58:22 +0100

    fixed up venv + figuring out graphs for the data

---
+++ analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("ping_log.csv",
+                 names=["timestamp","ip","min","max","avg","std","count","failed"])
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+for ip in df["ip"].unique():
+    device_data = df[df["ip"] == ip]
+    plt.plot(device_data["timestamp"], device_data["avg"], label=ip)
+
+plt.xlabel("Time")
+plt.ylabel("Average Ping (ms)")
+plt.title("Ping Over Time")
+plt.legend()
+plt.show()
